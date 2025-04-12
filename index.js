0a7358f296102db27f2ed9478ccd63148240cdb1
Andreas Sousa <devandreas96@gmail.com>
2025-04-11 21:45:05 -0300

    test

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let menu = document.querySelector(".menu-hidden")
 let input = document.querySelector("input")
 let menu2 = document.querySelector(".menu")
 let header = document.querySelector("header")
+let cards = document.querySelector(".cards")
 menu.addEventListener("click", entradadomouse)
+cards.addEventListener("click", abrircard)
+
+function abrircard(){
+    let criardiv = document.createElement("div", "paipai")
+}
+
 
 function entradadomouse(){
     if(menu2.style.display == "flex" ){
