dff519795e137e16fe7bf377ec23d52dfa6ad0c3
Andreas Sousa <devandreas96@gmail.com>
2025-04-12 01:20:49 -0300

    testes falhos 1

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ let menu = document.querySelector(".menu-hidden")
 let input = document.querySelector("input")
 let menu2 = document.querySelector(".menu")
 let header = document.querySelector("header")
-let cards = document.querySelector(".cards")
-menu.addEventListener("click", entradadomouse)
-cards.addEventListener("click", abrircard)
+let cards = document.querySelectorAll(".cards")
+menu.addEventListener("click", abrirmenu)
+menu.addEventListener("click", trocarcordois)
 
-function abrircard(){
-    let criardiv = document.createElement("div", "paipai")
+function trocarcordois(selected){
+    // selected.target.style.backgroundColor = "black"
+    let divComprar = document.createElement("div")
+    divComprar.classList.add("buy-page")
+    document.body.appendChild(divComprar)
+}
+
+for(let i = 0; i <= cards.length; i++ ){
+    cards[i].addEventListener("click", trocarcordois)
 }
 
 
-function entradadomouse(){
+function abrirmenu(){
+    
     if(menu2.style.display == "flex" ){
         menu2.style.display = "none"
         menu.style.transform = "rotateY(0deg)"
